9bdfec74f79324c7bc74e878dbac7cb9d934eeb6
sudddy <34531983+sudddy@users.noreply.github.com>
2021-04-13 16:11:34 +1200

    Removed commmented code

--- js/background.js
+++ js/background.js
@@ -2,20 +2,4 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
     });
   });
-
-
-
- 
-  //do this in global scope for popup.js
-
-  // chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-  //   chrome.declarativeContent.onPageChanged.addRules([{
-  //     conditions: [new chrome.declarativeContent.PageStateMatcher({
-  //       pageUrl: {hostEquals: 'developer.chrome.com'},
-  //     })
-  //     ],
-  //         actions: [new chrome.declarativeContent.ShowPageAction()]
-  //   }]);
-  // });
-
  
